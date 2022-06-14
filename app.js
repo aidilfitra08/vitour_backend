1c348bd6c19f2420480836042dca6800fe7df4bd
aidilfitra08 <aidil.gitu1@gmail.com>
2022-06-14 09:30:31 +0700

    add secretKey, corsOptions, and handling error

--- app.js
+++ app.js
@@ -2,15 +2,34 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const cors = require("cors");
 const dotenv = require("dotenv");
+const jwt = require("jsonwebtoken");
 
 const app = express();
+// const {isSignedIn}=require("./app/controllers/auth");
+
+app.set('secretKey', process.env.SECRET_KEY);
+
 dotenv.config();
+
+//Local
 // var corsOptions = {
-//   origin: "https://vitour-backend.herokuapp.com"
+//   origin: [
+//     'http://localhost:3004',  // admin website
+//     'http://localhost:3000',  // main website
+//   ],
+//   credentials: true,
 // };
 
-// app.use(cors(corsOptions));
-app.use(cors());
+//Online
+var corsOptions = {
+  origin: [
+    'https://vitour-crud.vercel.app',  // admin website
+    'https://vitour.herokuapp.com/',  // main website
+  ],
+  credentials: true,
+};
+
+app.use(cors(corsOptions));
 
 // parse requests of content-type - application/json
 app.use(bodyParser.json());
@@ -40,8 +59,27 @@ require("./app/routes/marketplace")(app);
 require("./app/routes/destination")(app);
 require("./app/routes/merchandise")(app);
 require("./app/routes/videovr")(app);
-require("./app/routes/admin")(app);
 require("./app/routes/searchbar")(app);
+var adminRoutes = require("./app/routes/admin");
+
+app.use("/api", adminRoutes);
+
+// express doesn't consider not found 404 as an error so we need to handle 404 explicitly
+// handle 404 error
+app.use(function(req, res, next) {
+  let err = new Error('Not Found');
+     err.status = 404;
+     next(err);
+ });
+ // handle errors
+ app.use(function(err, req, res, next) {
+  console.log(err);
+  
+   if(err.status === 404)
+    res.status(404).json({message: "Not found"});
+   else 
+     res.status(500).json({message: "Something looks wrong :( !!!"});
+ });
 
 // set port, listen for requests
 const PORT = process.env.PORT || 8080;
