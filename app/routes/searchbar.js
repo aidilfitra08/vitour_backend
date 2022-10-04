e0d96161bfda988ef1e85e74f60496f4501604b5
raihanfatoni <raihanfatoni23@gmail.com>
2022-10-04 14:41:08 +0700

    update

--- app/routes/searchbar.js
+++ app/routes/searchbar.js
@@ -2,27 +2,8 @@ module.exports = app => {
     const search = require("../controllers/searchbar.js");
   
     var router = require("express").Router();
-  
-    // Create a new Tutorial
-    // router.post("/", city.create);
-  
-    // Retrieve all city
+
     router.get("/", search.search);
-  
-    // Retrieve all published Tutorials
-    // router.get("/published", tutorials.findAllPublished);
-  
-    // // Retrieve a single Tutorial with id
-    // router.get("/:id", city.findOne);
-  
-    // // // Update a Tutorial with id
-    // router.put("/:id", city.update);
-  
-    // // // Delete a Tutorial with id
-    // router.delete("/:id", city.delete);
-  
-    // // Delete all Tutorials
-    // router.delete("/", tutorials.deleteAll);
   
     app.use("/api/search", router);
   };
