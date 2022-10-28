92f81ca6cd48d0f92ad57eecaef6eb6eca1d6ab5
aidilfitra08 <aidil.gitu1@gmail.com>
2022-10-28 17:53:01 +0700

    split admin and user auth

--- app/middleware/auth.js
+++ app/middleware/auth.js
@@ -2,7 +2,7 @@ const jwt = require('jsonwebtoken');
 const db = require("../models");
 const User = db.admin;
 
-module.exports = async (req, res, next) => {
+exports.adminPage = async (req, res, next) => {
   try {
     const token = req.headers.authorization.split(' ')[1];
     const decodedToken = jwt.verify(token, process.env.SECRET_KEY);
@@ -14,6 +14,32 @@ module.exports = async (req, res, next) => {
       throw 'Invalid user ID';
     } else {
       if (role == 'admin'){
+        next();
+      } else{
+        throw 'Action does not allowed'
+      }
+      
+    }
+  } catch {
+    res.status(401).json({
+      error: 'User Does Not Allowed to do This Action'
+    });
+  }
+};
+
+exports.webPage = async (req, res, next) => {
+  try {
+    const token = req.headers.authorization.split(' ')[1];
+    const decodedToken = jwt.verify(token, process.env.SECRET_KEY);
+    const user_id = decodedToken.user_id;
+    const role = decodedToken.role;
+
+    const user = await User.findOne({ where: user_id });
+    if (!user) {
+      throw 'Invalid user ID';
+    } else {
+      if (role == 'admin' || role == 'user'){
+        req.user_id_loggedin = user_id
         next();
       } else{
         throw 'Action does not allowed'
