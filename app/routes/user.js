22a42ab46b9ff76b84aaa35b035aecef2e29db4f
aidilfitra08 <aidil.gitu1@gmail.com>
2022-12-02 14:23:23 +0700

    update comments

--- app/routes/user.js
+++ app/routes/user.js
@@ -14,6 +14,7 @@ module.exports = app => {
   router.put("/my-profile", auth.webPage, user.updateProfile);
   router.put("/my-password", auth.webPage, user.updatePassword);
 
+  // router admin
   router.get("/user", auth.adminPage, user.getAllUser);
   router.get("/user/:id", auth.adminPage, user.getUserDetail);
   router.put("/user/:id", auth.adminPage, user.updateUser);
